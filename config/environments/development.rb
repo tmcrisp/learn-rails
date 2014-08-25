aee196d8923e1fb4feaa72caa7630837a1978ddb
Travis Crisp <tmcrisp@gmail.com>
2014-08-25 21:29:52 +0000

    sending mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
   
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -36,7 +37,7 @@ Rails.application.configure do
     username: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
     }
-  
+    
   config.action_mailer.perform_deliveries = true
 
   # Adds additional error checking when serving assets at runtime.
